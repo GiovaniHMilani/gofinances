e5830249426b9175c27df7a053828816b9484611
Giovani Milani <giovani.milani@ze.delivery>
2021-05-29 20:05:08 -0300

    feat: finalize header with icon to logout

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Container, Title } from './styles'
+import React from "react";
+
+import {
+  Container,
+  HeaderContainer,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon
+} from "./styles";
 
 export function Dashboard() {
-  return(
+  return (
     <Container>
-      <Title>Dashboard</Title>
+      <HeaderContainer>
+      <UserWrapper>
+        <UserInfo>
+          <Photo
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/48066516",
+            }}
+          />
+          <User>
+            <UserGreeting>Olá, </UserGreeting>
+            <UserName>Giovani</UserName>
+          </User>
+        </UserInfo>
+        <Icon name="power" />
+      </UserWrapper>
+    </HeaderContainer>
     </Container>
-  )
+  );
 }
