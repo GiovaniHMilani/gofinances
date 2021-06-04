540ec5fd5173c9eed4792d4f16a926a3ece38acd
Giovani Milani <giovani.milani@ze.delivery>
2021-06-04 12:27:55 -0300

    feat: add react-hook-form to project

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { CategorySelect } from "../CategorySelect";
-import { Modal } from "react-native";
+import { useForm } from "react-hook-form";
+import { Keyboard, Modal, TouchableWithoutFeedback } from "react-native";
 
 import {
   Container,
@@ -11,65 +12,101 @@ import {
   TransactionTypes,
 } from "./styles";
 
-import { Input } from "../../components/Form/Input";
+interface FormData {
+  name: string;
+  amount: string;
+}
+
+import { InputForm } from "../../components/Form/InputForm";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 
-import { Category } from '../CategorySelect'
+import { Category } from "../CategorySelect";
 
 export function Register() {
+  const { control, handleSubmit } = useForm();
+
   const [category, setCategory] = useState({
     key: "category",
     name: "Categoria",
   });
 
-  const [typeTransaction, setTypeTransaction] =
+  const [transactionType, setTransactionType] =
     useState<"UP" | "DOWN" | "">("");
+
   const [modalCategory, setModalCategory] = useState(false);
 
   function handleChangeCategory(isCategory: Category) {
     setCategory(isCategory);
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    };
+
+    console.log(data);
+  }
+
   return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-      <Form>
-        <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Preço" />
-          <TransactionTypes>
-            <TransactionTypeButton
-              onPress={() => setTypeTransaction("UP")}
-              type="UP"
-              title="Entrada"
-              isActive={typeTransaction === "UP"}
-              activeOpacity={0.7}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+        <Form>
+          <Fields>
+            <InputForm
+              control={control}
+              name="name"
+              placeholder="Nome"
+              autoCorrect={false}
+              autoCapitalize="sentences"
             />
-            <TransactionTypeButton
-              onPress={() => setTypeTransaction("DOWN")}
-              type="DOWN"
-              title="Saída"
-              isActive={typeTransaction === "DOWN"}
+            <InputForm
+              control={control}
+              name="amount"
+              placeholder="Preço"
+              keyboardType="numeric"
             />
-          </TransactionTypes>
-          <CategorySelectButton
-            onPress={() => setModalCategory(true)}
-            title={category.name}
+            <TransactionTypes>
+              <TransactionTypeButton
+                onPress={() => setTransactionType("UP")}
+                type="UP"
+                title="Entrada"
+                isActive={transactionType === "UP"}
+                activeOpacity={0.7}
+              />
+              <TransactionTypeButton
+                onPress={() => setTransactionType("DOWN")}
+                type="DOWN"
+                title="Saída"
+                isActive={transactionType === "DOWN"}
+              />
+            </TransactionTypes>
+            <CategorySelectButton
+              onPress={() => setModalCategory(true)}
+              title={category.name}
+            />
+          </Fields>
+          <Button
+            text="Enviar"
+            onPress={handleSubmit(handleRegister)}
+            activeOpacity={0.7}
           />
-        </Fields>
-        <Button text="Enviar" activeOpacity={0.7} />
-      </Form>
-      <Modal animationType="slide" visible={modalCategory}>
-        <CategorySelect
-          category={category}
-          setCategory={handleChangeCategory}
-          closeSelectCategory={() => setModalCategory(false)}
-        />
-      </Modal>
-    </Container>
+        </Form>
+        <Modal animationType="slide" visible={modalCategory}>
+          <CategorySelect
+            category={category}
+            setCategory={handleChangeCategory}
+            closeSelectCategory={() => setModalCategory(false)}
+          />
+        </Modal>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
